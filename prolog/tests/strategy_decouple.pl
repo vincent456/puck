dc7b19abaad58198af518f6e95a46400d76a6ad8
Loïc Girault <loic.girault@lip6.fr>
2014-01-27 19:28:56 +0100

    first step of a working solver

---
+++ prolog/tests/strategy_decouple.pl
@@ -0,0 +1,5 @@
+hideScopeSetFrom(
+	['strategy.candidate.SortingContext.bubbleSort__@primitive.int', 
+	 'strategy.candidate.SortingContext.linearSort__@primitive.int'], 
+	['strategy.candidate.SortingContext']).
+
