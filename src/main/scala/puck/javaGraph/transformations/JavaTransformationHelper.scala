48e13c7a17c7ee734b377da1c6adb21ec3ff4d6c
Loïc Girault <loic.girault@lip6.fr>
2015-04-10 17:53:30 +0200

    missing file

---
+++ src/main/scala/puck/javaGraph/transformations/JavaTransformationHelper.scala
@@ -0,0 +1,34 @@
+package puck.javaGraph
+package transformations
+
+import puck.graph._
+import puck.graph.transformations.MergeMatcherInstances
+import puck.graph.transformations.rules.MergingCandidatesFinder
+import nodeKind.Interface
+
+/**
+ * Created by lorilan on 4/9/15.
+ */
+object JavaTransformationHelper extends MergingCandidatesFinder {
+
+  def mergeMatcherInstances : MergeMatcherInstances =
+    JavaMergeMatcherInstances
+
+  override def find(g : DependencyGraph, node : ConcreteNode) : Option[ConcreteNode] = {
+
+    val nid = node.id
+    node.kind match {
+      case Interface if g.content(nid).nonEmpty =>
+        g.concreteNodes.find { other =>
+          node.canBeMergedInto(other, g) &&
+            g.usersOf(nid).forall(!g.interloperOf(_,other.id)) &&
+            g.usedBy(nid).forall(!g.interloperOf(other.id, _)
+            )
+        }
+      case _ => None
+    }
+
+  }
+  def findIn(g : DependencyGraph, method : ConcreteNode, interface : ConcreteNode) : Option[NodeId] =
+    InterfaceMergeMatcher.findMergingCandidateIn(g, method, interface)
+}
