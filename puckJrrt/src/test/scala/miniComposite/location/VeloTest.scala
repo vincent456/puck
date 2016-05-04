0726d38c609a40facb7ae77d3eb92506b35abad2
ziane <mikal.ziane@gmail.com>
2016-05-04 15:08:20 +0200

    test vélo

---
+++ puckJrrt/src/test/scala/miniComposite/location/VeloTest.scala
@@ -0,0 +1,26 @@
+package miniComposite.location
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by mikal on 04/05/2016.
+  */
+class VeloTest extends FunSuite {
+
+  test("testSetUtilisateur") {
+
+  }
+
+  test("testGetUtilisateur") {
+
+  }
+
+  test("testRendre") {
+
+  }
+
+  test("testGetId") {
+
+  }
+
+}
