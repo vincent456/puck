8e12545a56d70a3fdc4300249d300e2f9a914e0e
Loïc Girault <loic.girault@lip6.fr>
2016-12-02 10:55:02 +0100

    identified constraint evaluation or graph building error under test

--- puckExtendJ/src/test/scala/puck/javaGraph/dependencyRules/DependencyRulesSpec.scala
+++ puckExtendJ/src/test/scala/puck/javaGraph/dependencyRules/DependencyRulesSpec.scala
@@ -33,8 +33,45 @@ class DependencyRulesSpec extends AcceptanceSpec {
 //    }
 
     cts.violations(graph) should not be empty
+  }
 
+  scenario("error from a marauroa constraint generated example") {
+    val s = new ScenarioFactory(
+      """package p.pa;
+        |import p.pb.B;
+        |import p.pb.C;
+        |
+        |public class A {
+        | void ontInit() throws Exception {
+        |   B.get().get(C.class).load(this);
+        | }
+        |}""",
 
+        """package p.pb;
+          |public class B {
+          | public static B get(){return null;}
+          | public <T extends java.lang.Object> T get(Class<T> clazz) { return null;}
+          |}
+        """,
+      """
+        |package p.pb;
+        |import p.pa.A;
+        |public class C {
+        | public void load(A a){}
+        |}
+      """
+     )
+
+    import s._
+    val cts = s.parseConstraint("hide [p.pb] from [p.pa]")
+
+    //    import puck.graph.ShowDG._
+    //    cts.violations(graph).foreach {
+    //      u =>
+    //      (graph, u).println
+    //    }
+
+    cts.violations(graph) should not be empty
   }
 
 }
