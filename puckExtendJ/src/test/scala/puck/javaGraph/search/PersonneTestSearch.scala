f63bf5fe29d30703bbd6339d8f042fd7b33a5583
puckDistrib <cedric.besse@gmail.com>
2017-05-05 15:59:15 +0200

    "repared" nano and personne examples

--- puckExtendJ/src/test/scala/puck/javaGraph/search/PersonneTestSearch.scala
+++ puckExtendJ/src/test/scala/puck/javaGraph/search/PersonneTestSearch.scala
@@ -2,12 +2,14 @@ package puck.javaGraph.search
 
 import java.io.File
 
-import puck.TestUtils._
-import puck.graph.constraints.search.DecoratedGraphEvaluator
+import puck.graph.constraints.ConstraintsMapsUtils._
+import puck.graph.constraints.search.{ControlWithHeuristic, DecoratedGraphEvaluator, WithVirtualNodes}
 import puck.graph.{ConcreteNode, _}
-import puck.javaGraph.ScenarioFactory
-import puck.search.AStarSearchOrdering
-import org.extendj.ExtendJGraphUtils.dotHelper
+import puck.javaGraph.{ScenarioFactory, SearchEngineWithLoggedFitness}
+import puck.search.{AStarSearchOrdering, SearchControl}
+import org.extendj.ExtendJGraphUtils.{Rules, dotHelper, violationsKindPriority}
+import puck.javaGraph.search.NanoTestSearch.path
+import puck.javaGraph.search.SearchTest.solsDir
 
 /**
   * Created by cedric on 18/05/2016.
@@ -22,7 +24,7 @@ object PersonneTestSearch {
     val filePaths = Seq(
       s"$path/personne/Personne.java",
       s"$path/personne/Client.java")
-    val scenario = new ScenarioFactory(filePaths:_*)
+    val scenario = new ScenarioFactory(filePaths: _*)
 
     val constraints = scenario.parseConstraintsFile(s"$path/decouple.wld")
 
@@ -31,20 +33,25 @@ object PersonneTestSearch {
     //        Some(100),Some(5))
 
 
-    val fitness1: DependencyGraph => Double =
-      Metrics.fitness1(_, constraints, 1, 1).toDouble
+    val nodesSet = scenario.graph nodesIn constraints
 
-    val evaluator = DecoratedGraphEvaluator.equalityByMapping[Option[ConcreteNode]](fitness1)
-    val strategy = new AStarSearchStrategyGraphDisplayOnly[Option[ConcreteNode]](
+    val fitness2: DependencyGraph => Double =
+      Metrics.fitness2(_, nodesSet).toDouble
+
+    val evaluator = DecoratedGraphEvaluator.equalityByMapping[Any](fitness2)
+    val strategy = new AStarSearchStrategyGraphDisplayOnly[Any](
       evaluator, Some(constraints),
-      10, 1000, outDir)
+      10, 1000, solsDir)
 
-    implicit val ordering = new AStarSearchOrdering(evaluator)
+    val control = new ControlWithHeuristic(Rules, scenario.graph.newGraph(mutabilitySet = scenario.initialMutability),
+      constraints, WithVirtualNodes, violationsKindPriority).asInstanceOf[SearchControl[DecoratedGraph[Any]]]
 
-    val res = solveAllBlind(scenario.graph, constraints,
-      scenario.initialMutability, strategy, Some(1))
+    // SearchEngine(strategy, control, Some(1)) :  Some(n) => n sol(s), None => all sols
+    val engine = new SearchEngineWithLoggedFitness(strategy, control, constraints)
+    engine.explore()
 
-    SearchTest.printResult(res, ordering, scenario.fullName2id, filePaths:_*)
-
+    SearchTest.printResult(engine.successes,
+      engine.searchStrategy.SearchStateOrdering,
+      scenario.fullName2id, constraints, filePaths: _*)
+    }
   }
-}
